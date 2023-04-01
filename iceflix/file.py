7cbaa53867bde82efa92d362300c492119bfcc97
Luis <Luis.Molina1@alu.uclm.es>
2023-04-01 13:42:28 +0200

    Definición de interfaces

--- iceflix/file.py
+++ iceflix/file.py
@@ -12,7 +12,6 @@ import os
 import Ice
 import IceFlix
 
-
 #------------------------------------------------
 #                   FileHandler
 #------------------------------------------------
@@ -26,10 +25,32 @@ class FileHandler(IceFlix.FileHandler):
 
     def receive(self, size, userToken, current=None):
         """Receive the specified number of bytes from the file"""
+        """Can throws -> Unauthorized"""
         #To Do
         return null
 
     def close(self, userToken, current=None):
         """Notify the server that the proxy for this file, won´t be used and will be removed"""
+        """Can throws -> Unauthorized"""
         #To Do
         return null
+
+#------------------------------------------------
+#                   FileService
+#------------------------------------------------
+class FileService(IceFlix.FileService):
+
+    def __init__(self, path):
+        """Initialize parameters"""
+
+    def openFile(self, mediaId, userToken, current=None):
+        """"Given the media identifier, it will return a proxy to the file handler (FileHandler), which will enable downloading it"""
+        """Can throws -> Unauthorized, WrongMediaId"""
+
+    def uploadFile(self, uploader, adminToken, current=None):
+        """Given the administrator token and a proxy for uploading files, it will store it in the directory and returns its identifier"""
+        """Can throws -> Unauthorized"""
+
+    def removeFile(self, mediaId, adminToken, current=None):
+        """Give the identifier and the administrator token"""
+        """Can throws -> Unauthorized, WrongMediaId"""
