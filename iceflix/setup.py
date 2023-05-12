827daed3e5553d3d46f6ed3ccba144acd0a1c346
Luis <Luis.Molina1@alu.uclm.es>
2023-05-12 13:56:46 +0200

    Añadido setup.py para usar pip install

---
+++ iceflix/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="iceflix_fileservice",
+    version="0.1",
+    packages=find_packages(),
+    author="Luis",
+    author_email="Luis.Molina1@alu.uclm.es",
+    description="Implementación del servicio de ficheros Iceflix",
+    url="https://github.com/molinem/ICEFLIX-FILESERVICE.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
